3ab1c99c982ebd24bb33b448ecadb474ca8382d5
rohit290799 <86189618+rohit290799@users.noreply.github.com>
2022-11-14 23:34:57 +0530

    Add files via upload

---
+++ petals.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def function(v):
+  if v > 0:
+   return (5*80.0/v)
+  else:
+    return 0
